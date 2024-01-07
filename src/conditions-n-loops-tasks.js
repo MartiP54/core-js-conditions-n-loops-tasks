ab90440851992ad616455a9621a3f5088d3d2ee0
MartiP54 <116718918+MartiP54@users.noreply.github.com>
2024-01-07 20:16:06 +0700

    feat: add 12 task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -396,8 +396,27 @@ function getSpiralMatrix(size) {
  *    [7, 8, 9]         [9, 6, 3]
  *  ]                 ]
  */
-function rotateMatrix(/* matrix */) {
-  throw new Error('Not implemented');
+function rotateMatrix(matrix) {
+  const rezmatrix = [];
+  const rezmatrix1 = matrix;
+  for (let m = 0; m < matrix.length; m += 1) {
+    rezmatrix[m] = [];
+    for (let n = 0; n < matrix.length; n += 1) {
+      rezmatrix[m][n] = 0;
+    }
+  }
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix.length; j += 1) {
+      rezmatrix[j][matrix.length - 1 - i] = matrix[i][j];
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix.length; j += 1) {
+      rezmatrix1[i][j] = rezmatrix[i][j];
+    }
+  }
+  return matrix;
 }
 
 /**
